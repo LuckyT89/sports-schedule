c72c13ebb01c92afa02e6eb47e504ced366b3889
TylerL <tluckewicz@gmail.com>
2021-06-10 09:24:18 -0400

    set up initial fetch request

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import AdminCard from './components/AdminCard/AdminCard';
 import Home from './components/Home/Home';
 import HomeCard from './components/HomeCard/HomeCard';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/toys')
+      .then(res => res.json())
+      .then(toys => console.log(toys))
+  }, []);
+
+
+
   return (
     <div>
       <Home />
