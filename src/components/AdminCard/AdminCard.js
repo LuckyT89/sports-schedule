0b26478078603cc0b1462c88a11d5b981154a702
TylerL <tluckewicz@gmail.com>
2021-06-11 08:57:41 -0400

    start canceled checkbox functionality

--- src/components/AdminCard/AdminCard.js
+++ src/components/AdminCard/AdminCard.js
@@ -6,13 +6,18 @@ function AdminCard({ game, removeGame }) {
         removeGame(game.id);
     }
 
+    function handleCanceled() {
+        console.log('toggle that!');
+    }
+
     return (
         <div className='admin-card'>
             <p>{game.date}</p>
             <p>{game.time}</p>
             <p>{game.homeTeam} vs {game.awayTeam}</p>
-            <button onClick={handleRemoveGame} >Delete</button>
-            <button>Canceled</button>
+            <button onClick={handleRemoveGame} >Remove</button>
+            <label>Mark canceled<input type='checkbox' checked={game.isCanceled ? 'checked' : ''} onChange={handleCanceled} ></input></label>
+            <p>This game is {game.isCanceled ? "canceled" : "NOT canceled"} </p>
         </div>
     );
 }
