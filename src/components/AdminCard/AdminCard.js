b9f2589a021cad4cef095a3ecc2725ea573904d7
TylerL <tluckewicz@gmail.com>
2021-06-11 14:10:20 -0400

    import images into AdminCard component

--- src/components/AdminCard/AdminCard.js
+++ src/components/AdminCard/AdminCard.js
@@ -1,5 +1,14 @@
 import './AdminCard.css';
+import bears from '../../img/bears.jpg';
+import bulls from '../../img/bulls.jpg';
+import eagles from '../../img/eagles.jpg';
+import panthers from '../../img/panthers.jpg';
+import rhinos from '../../img/rhinos.jpg';
+import sharks from '../../img/sharks.jpg';
+import tigers from '../../img/tigers.jpg';
+import wolves from '../../img/wolves.jpg';
 import { useState, } from 'react';
+
 
 
 function AdminCard({ game, removeGame, cancelGame }) {
@@ -32,6 +41,7 @@ function AdminCard({ game, removeGame, cancelGame }) {
             <p>{game.date}</p>
             <p>{game.time}</p>
             <p>{game.homeTeam} vs {game.awayTeam}</p>
+            <p><img src={tigers} alt='logo' /> Vs. <img src={wolves} alt='logo' /></p>
             <button onClick={handleRemoveGame} >Remove</button>
             <label>Mark canceled<input type='checkbox' checked={cancelStatus ? 'checked' : ''} onChange={handleCanceled} ></input></label>
             <p className='canceled'>This game is {game.isCanceled ? "canceled" : "NOT canceled"} </p>
