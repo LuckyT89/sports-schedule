9be2f6debe0ca6a1f6e02d7188fd2aa5c6bfb75e
TylerL <tluckewicz@gmail.com>
2021-06-11 14:06:12 -0400

    import images into HomeCard component

--- src/components/HomeCard/HomeCard.js
+++ src/components/HomeCard/HomeCard.js
@@ -1,5 +1,12 @@
 import './HomeCard.css';
-import bears from '../../img/bulls.jpg';
+import bears from '../../img/bears.jpg';
+import bulls from '../../img/bulls.jpg';
+import eagles from '../../img/eagles.jpg';
+import panthers from '../../img/panthers.jpg';
+import rhinos from '../../img/rhinos.jpg';
+import sharks from '../../img/sharks.jpg';
+import tigers from '../../img/tigers.jpg';
+import wolves from '../../img/wolves.jpg';
 
 function HomeCard({ game }) {
     return (
@@ -8,7 +15,7 @@ function HomeCard({ game }) {
             <p>{game.date}</p>
             <p>{game.time}</p>
             <p>{game.homeTeam} vs {game.awayTeam}</p>
-            <img src={bears} alt='logo' />
+            <p><img src={tigers} alt='logo' /> Vs. <img src={wolves} alt='logo' /></p>
         </div>
     );
 }
