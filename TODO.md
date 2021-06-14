edc9316af9e0e2472fe7fc2f61dab1ef119d9d58
TylerL <tluckewicz@gmail.com>
2021-06-13 22:22:37 -0400

    add credit links for images used

--- TODO.md
+++ TODO.md
@@ -1,4 +1,2 @@
-
--add credit links to artist
 -add ternanry in case there are no games
 -make 404 page
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,9 @@ function Home({ games }) {
     return (
         <div className='home p-3 bg-dark'>
             {homeCards}
+            <a href="https://www.freepik.com/vectors/badge">Badge vector created by macrovector</a>
+            <a href='https://www.freepik.com/vectors/star'>Star vector created by macrovector</a>
+            <a href='https://www.freepik.com/vectors/logo'>Logo vector created by freepik</a>
         </div>
     );
 }
